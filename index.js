438f95b74c7a0eef7c0d644f5b77398ff0ae3f9d
soizxs <oogg41008@gmail.com>
2026-09-15 18:00:35 +0300

    Add bot code

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`البوت اشتغل: ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
